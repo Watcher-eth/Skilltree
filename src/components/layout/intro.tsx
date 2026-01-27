bfa1053be5013b0719b7b4a8e02de59c44144e5f
Watcher <watchereth10@gmail.com>
2026-01-26 20:11:27 -0800

    auth fixes

--- src/components/layout/intro.tsx
+++ src/components/layout/intro.tsx
@@ -252,7 +252,7 @@ function SecondaryButton({
       type="button"
       onClick={onClick}
       className={[
-        "w-full flex items-center rounded-2xl border border-[#E5E5EA] bg-white px-6 py-3 font-semibold",
+        "w-full flex items-center justify-center rounded-2xl border border-[#E5E5EA] bg-white px-6 py-3 font-semibold",
         UI.buttonText,
         "text-[#1C1C1E]",
         "transition-[border-color,box-shadow,transform] duration-150",
@@ -260,7 +260,7 @@ function SecondaryButton({
         "focus:outline-none focus-visible:shadow-[0_0_0_4px_rgba(10,132,255,0.20)]",
       ].join(" ")}
     >
-      {icon && <img src={icon} alt="" className="w-5 h-5" />}
+      {icon && <img src={icon} alt="" className="w-5 h-5 mr-1" />}
       {children}
     </button>
   );
