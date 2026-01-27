9ca7b4d35a7260b67c69532dc7546ef93fcfc787
Watcher <watchereth10@gmail.com>
2026-01-26 20:13:46 -0800

    fixes

--- src/components/layout/intro.tsx
+++ src/components/layout/intro.tsx
@@ -260,7 +260,7 @@ function SecondaryButton({
         "focus:outline-none focus-visible:shadow-[0_0_0_4px_rgba(10,132,255,0.20)]",
       ].join(" ")}
     >
-      {icon && <img src={icon} alt="" className="w-5 h-5 mr-1" />}
+      {icon && <img src={icon} alt="" className="w-5 h-5 mr-2" />}
       {children}
     </button>
   );
